031628c89ce392af1e70a8bbdcf702c6e1a09e16
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-18 16:46:28 +0900

    Add more UI

--- example/index.ts
+++ example/index.ts
@@ -15,6 +15,16 @@ let dragging = false
 let lastX = 0
 let lastY = 0
 
+let allowedGap = 0
+
+function clearDrawing() {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function clearFloodFill() {
+  floodfillContext.clearRect(0, 0, floodfillCanvas.width, floodfillCanvas.height)
+}
+
 canvas.addEventListener("pointerdown", e => {
   if (e.shiftKey) {
     const data = context.getImageData(0, 0, canvas.width, canvas.height)
@@ -24,12 +34,12 @@ canvas.addEventListener("pointerdown", e => {
     const y = Math.round(e.offsetY + 0.5)
     console.time("floodFill")
     //floodFill(x, y, src, dst)
-    floodFillWithGap(x, y, 10, src, dst)
+    floodFillWithGap(x, y, allowedGap, src, dst)
     console.timeEnd("floodFill")
     dst.toImageData(new Uint8ClampedArray([0,0,0,0]), new Uint8ClampedArray([0,0,255,255]), data)
     floodfillContext.putImageData(data, 0, 0)
   } else {
-    floodfillContext.clearRect(0, 0, floodfillCanvas.width, floodfillCanvas.height)
+    clearFloodFill()
     dragging = true
     lastX = e.offsetX
     lastY = e.offsetY
@@ -50,3 +60,14 @@ canvas.addEventListener("pointerup", e => {
   dragging = false
 })
 
+const allowedGapSpan = document.querySelector("#allowed-gap") as HTMLSpanElement
+const allowedGapInput = document.querySelector("#allowed-gap-input") as HTMLInputElement
+const clearDrawingButton = document.querySelector("#clear-drawing") as HTMLButtonElement
+const clearFloodFillButton = document.querySelector("#clear-flood-fill") as HTMLButtonElement
+
+allowedGapInput.addEventListener("change", () => {
+  allowedGap = parseInt(allowedGapInput.value)
+  allowedGapSpan.innerText = String(allowedGap)
+})
+clearDrawingButton.addEventListener("click", clearDrawing)
+clearFloodFillButton.addEventListener("click", clearFloodFill)
