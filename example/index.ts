0a6b8ce091cf1b2b597b6bec4f364b9892dd76b0
Ryohei Ikegami <iofg2100@gmail.com>
2016-12-18 14:11:24 +0900

    Draw on example

--- example/index.ts
+++ example/index.ts
@@ -1,1 +1,31 @@
-console.log("test")
+const canvas = document.querySelector("#canvas") as HTMLCanvasElement
+const context = canvas.getContext("2d")!
+
+context.strokeStyle = "black"
+context.lineWidth = 4
+context.lineCap = "round"
+
+let dragging = false
+let lastX = 0
+let lastY = 0
+
+canvas.addEventListener("pointerdown", e => {
+  dragging = true
+  lastX = e.offsetX
+  lastY = e.offsetY
+  canvas.setPointerCapture(e.pointerId)
+})
+canvas.addEventListener("pointermove", e => {
+  if (dragging) {
+    context.beginPath()
+    context.moveTo(lastX, lastY)
+    context.lineTo(e.offsetX, e.offsetY)
+    lastX = e.offsetX
+    lastY = e.offsetY
+    context.stroke()
+  }
+})
+canvas.addEventListener("pointerup", e => {
+  dragging = false
+})
+
